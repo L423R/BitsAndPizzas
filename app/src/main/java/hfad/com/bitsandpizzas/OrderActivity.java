616deaa091369a5f9480e16d579921794b5d7108
L423R <maxim.krylov704@yandex.ru>
2017-10-19 18:20:30 +0300

    Макет выдвижного меню(ANDROID)

---
+++ app/src/main/java/hfad/com/bitsandpizzas/OrderActivity.java
@@ -0,0 +1,16 @@
+package hfad.com.bitsandpizzas;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_activity);
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
